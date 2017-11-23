0904d220e00394a541fe264e6c800a700c75a209
João Paulo Nobrega <joao.paulo.nobrega@avanade.com>
2017-11-23 10:05:00 -0200

    Client list.

--- src/components/client/client-reducer.js
+++ src/components/client/client-reducer.js
@@ -2,6 +2,26 @@
 import * as types  from './client-action-types';
 
 const INITIAL_STATE = {
+    clientsRegistered: [
+        {
+            name: 'Pedro Carmona', 
+            email: 'pedrocarmado@gmail.com', 
+            phone: '+55 11 9999-22332', 
+            doc: '42 42424 42424 2', 
+        },     
+        {
+            name: 'Henrique Salomão', 
+            email: 'henquire@gmail.com', 
+            phone: '+55 11 99877-22332', 
+            doc: '323 323 3222', 
+        }, 
+        {
+            name: 'Rogerio Abreu', 
+            email: 'rogerio@gmail.com', 
+            phone: '+55 11 9954-2323', 
+            doc: '42 3232 232 32', 
+        }, 
+    ],
     client: {
         name: 'Jp Nobrega', 
         email: '', 
--- src/data.js
+++ src/data.js
@@ -6,19 +6,20 @@ import Web from 'material-ui/svg-icons/av/web';
 import {cyan600, pink600, purple600} from 'material-ui/styles/colors';
 import ExpandLess from 'material-ui/svg-icons/navigation/expand-less';
 import ExpandMore from 'material-ui/svg-icons/navigation/expand-more';
+import ContactIcon from 'material-ui/svg-icons/communication/contact-phone';
 import ChevronRight from 'material-ui/svg-icons/navigation/chevron-right';
 
 const data = {
   menus: [
     { text: 'DashBoard', icon: <Assessment/>, link: '/dashboard' },
-    { text: 'Cliente', icon: <Web/>, link: '/client/register', 
+    { text: 'Cliente', icon: <ContactIcon/>, link: '/client/register', 
       subMenu: [
-        { text: 'Registrar', icon: <Assessment/>, link: '/client/register' },
-        { text: 'Pesquisar', icon: <GridOn/>, link: '/client/register' }
+        { text: 'Cadastrados', icon: <GridOn/>, link: '/client/register' },
+        { text: 'Cadastrar', icon: <PermIdentity/>, link: '/client/register' }
       ] 
     },
     { text: 'Table Page', icon: <GridOn/>, link: '/table' },
-    { text: 'Login Page', icon: <PermIdentity/>, link: '/login' }
+    { text: 'Login Page', icon: <Web/>, link: '/login' }
   ],
   tablePage: {
     items: [
